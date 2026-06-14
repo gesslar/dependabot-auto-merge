34df13c811e886e39b858f8e56e18acd668c7892
gesslar <1266935+gesslar@users.noreply.github.com>
2026-06-14 15:17:27 -0400

    aroomahoo

--- dist/index.js
+++ dist/index.js
@@ -36282,7 +36282,7 @@ const mergeFlags = {
       const dependabotPRs = prs.filter(pr => pr.user.login === "dependabot[bot]")
       info(`Found ${dependabotPRs.length} open Dependabot PRs in ${repoOwner}/${repo}`)
 
-      // Comment on each PR
+      // Enable auto-merge on each PR
       for(const pr of dependabotPRs) {
         info(`→ PR #${pr.number}: ${pr.title}`)
 
